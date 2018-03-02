7f0285e13d7e513bf6d820f7f842a0c0e472ee83
Daniel B <dantheman41@live.com.au>
2018-03-02 21:33:55 +1100

    Started cards project. Going well. Expanding of cards commented out.

---
+++ public/js/controller/core.js
@@ -0,0 +1,34 @@
+app.controller("core", function($scope) {
+	$scope.search = ""
+
+	$scope.searchFilter = function(card) {
+		if ($scope.search.length == 0)
+			return true;
+
+		var searchWords = $scope.search.toLowerCase().split(" ");
+		var cardTitleWords = card.title.toLowerCase().split(" ");
+		var cardTags = card.tags;
+		for (var n = 0; n < searchWords.length; n++){
+			for (var i = 0; i < cardTitleWords.length; i++){
+				if (cardTitleWords[i].indexOf(searchWords[n]) != -1){
+					return true;
+				}
+			}
+			for (var i = 0; i < cardTags.length; i++){
+				if (cardTags[i].indexOf(searchWords[n].toLowerCase()) != -1){
+					return true;
+				}
+			}
+		}
+		return false;
+	};
+
+	$scope.cards = [
+		{ img: "dna rna polymerase.jpg", title: "DNA RNA Polymerase Differences", tags:["DNA","RNA","polymerase","comparison","differences","replication","transcription"] },
+		{ img: "photosynthesis io.jpg", title: "Photosynthesis inputs and outputs", tags:["process","input","output"] },
+		{ img: "polymerisation.jpg", title: "Polymerisation", tags:["chemistry"] },
+		{ img: "protein structure.jpg", title: "Four levels of protein structure", tags:["protein","structure"] },
+		{ img: "enzyme substrate fit.jpg", title: "Lock and key versus induced fit model", tags:[] },
+		{ img: "endocytosis exocytosis.jpg", title: "Endocytosis versus exocytosis", tags:[] }
+	]
+});
---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(80, () => console.log('Cards app listening on port 80!'))
