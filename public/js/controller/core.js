c121754aa8db9ae49d6421f2632eb9100bfd22fa
Daniel B <dantheman41@live.com.au>
2018-04-04 11:57:11 +1000

    tried image uploading through 3 different methods. ended up with base64

--- public/js/controller/core.js
+++ public/js/controller/core.js
@@ -1,5 +1,35 @@
-app.controller("core", function($scope) {
+app.directive("fileread", [function () {
+	return {
+		scope: {
+			fileread: "="
+		},
+		link: function (scope, element, attributes) {
+			element.bind("change", function (changeEvent) {
+				var reader = new FileReader();
+				reader.onload = function (loadEvent) {
+					console.log(loadEvent.target.result)
+					scope.$apply(function () {
+						scope.fileread = loadEvent.target.result;
+					});
+				}
+				reader.readAsDataURL(changeEvent.target.files[0]);
+			});
+		}
+	}
+}]);
+app.controller("core", function($scope,$http) {
 	$scope.search = ""
+
+	$scope.newcard = {title:"",tags:"",file:undefined}
+
+	$scope.newCard = function () {
+		$http({
+			method  : 'POST',
+			url     : '/upload',
+			data    : $scope.newcard,  // pass in data as strings
+			headers : { 'Content-Type': "application/json" }  // set the headers so angular passing info as form data (not request payload)
+		})
+	}
 
 	$scope.searchFilter = function(card) {
 		if ($scope.search.length == 0)
--- server.js
+++ server.js
@@ -1,6 +1,44 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser")
+var path = require("path")
+var fs = require("fs")
+
+var multer  = require('multer')
+var upload = multer({ dest: 'tmp/' })
 
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended:true, limit: '50mb'}))
+app.use(bodyParser.json({limit: '50mb'}))
+
+// app.post('/upload', upload.single("file"), function (req, res) {
+app.post('/upload', function (req, res) {
+	// if (req.file == undefined){
+	// 	res.status(500).json({error: "No image."});
+	// 	return;
+	// }
+	// var tempPath = req.file.path,
+
+	var base64Data = req.body.file.replace(/^data:([A-Za-z-+/]+);base64,/, '');
+
+	var targetPath = path.resolve('./public/cards/'+req.body.title+".jpg");
+	fs.writeFile(targetPath, base64Data, 'base64', function(err) {
+		if(err) console.log("Error: "+err);
+	});
+
+	// fs.rename(tempPath, targetPath, function(err) {
+	// 	if (err) throw err;
+	// 	//Upload complete
+	// });
+
+	res.redirect("back")
+	// res.status(200).json({message: "Upload successful."});
+
+	// To delete
+	// fs.unlink(tempPath, function (err) {
+	// 	if (err) console.log(err);
+	// });
+});
 
 app.listen(8000, () => console.log('Cards app listening on port 8000!'))
